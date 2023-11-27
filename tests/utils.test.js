d7c780faf0b19c2738e2b33bcf072d0d4dfcd3fa
eremes7 <eetu.remes1997@gmail.com>
2023-11-27 20:34:03 +0200

    ci/cd config

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -13,6 +13,10 @@ describe('Config liittyen', () => {
         process.env = originalEnv
     })
     test('MONGODB_URI on määritelty', () => {
+        console.log(config.MONGODB_URI)
+        console.log(process.env.MONGODB_URI)
+        console.log(process.env.TEST_MONGO_URI)
+        console.log(config)
         expect(config.MONGODB_URI).toBeDefined()
     })
     test('PORT on määritelty', () => {
@@ -30,7 +34,7 @@ describe('Config liittyen', () => {
     test('testiympäristössä NODE_ENV on test', () => {
         process.env.NODE_ENV = 'test'
         const config = require('../utils/config')
-        expect(config.MONGODB_URI).toBe(process.env.TEST_MONGODB_URI)
+        expect(config.MONGODB_URI).toBe(process.env.TEST_MONGO_URI)
     })
     test('tuotantoympäristössä NODE_ENV on production', () => {
         process.env.NODE_ENV = 'production'
